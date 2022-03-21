f1ba10a80d06c8c3ab6950d9b8c34ea85f5af6c9
Daniel Bahrami <daniel@bahrami.dk>
2022-03-22 00:41:44 +0100

    Added JavaLab

---
+++ JavaLab/Collision/src/main/java/dk/sdu/mmmi/cbse/collision/Collision.java
@@ -0,0 +1,46 @@
+package dk.sdu.mmmi.cbse.collision;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+public class Collision implements IPostEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity entity : world.getEntities()) {
+            for (Entity collisionDetection : world.getEntities()) {
+                LifePart entityLife = entity.getPart(LifePart.class);
+                LifePart collisionLife = collisionDetection.getPart(LifePart.class);
+                if (entity.getID().equals(collisionDetection.getID())) {
+                    continue;
+                }
+                if (entityLife.getExpiration() <= 0) {
+                    world.removeEntity(entity);
+                    if (collisionLife.getExpiration() <= 0) {
+                        world.removeEntity(collisionDetection);
+                    }
+                }
+                if (this.Collides(entity, collisionDetection)) {
+                    if (entityLife.getLife() > 0) {
+                        entityLife.setLife(entityLife.getLife() - 1);
+                        if (entityLife.getLife() <= 0) {
+                            world.removeEntity(entity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public Boolean Collides(Entity entity, Entity entity2) {
+        PositionPart entMov = entity.getPart(PositionPart.class);
+        PositionPart entMov2 = entity2.getPart(PositionPart.class);
+        float dx = (float) entMov.getX() - (float) entMov2.getX();
+        float dy = (float) entMov.getY() - (float) entMov2.getY();
+        float distance = (float) Math.sqrt(dx * dx + dy * dy);
+        if (distance < (entity.getRadius() + entity2.getRadius())) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ JavaLab/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyControlSystem.java
@@ -0,0 +1,49 @@
+package dk.sdu.mmmi.cbse.enemy;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import java.util.Random;
+public class EnemyControlSystem implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity enemy : world.getEntities(Enemy.class)) {
+            PositionPart positionPart = enemy.getPart(PositionPart.class);
+            MovingPart movingPart = enemy.getPart(MovingPart.class);
+            LifePart lifePart = enemy.getPart(LifePart.class);
+            Random random = new Random();
+            float randomFloat = random.nextFloat();
+            movingPart.setUp(randomFloat > .1f && randomFloat < .9f);
+            movingPart.setLeft(randomFloat < .5f);
+            movingPart.setRight(randomFloat > .5f);
+            movingPart.process(gameData, enemy);
+            positionPart.process(gameData, enemy);
+            lifePart.process(gameData, enemy);
+            updateShape(enemy);
+            movingPart.setUp(false);
+            movingPart.setLeft(false);
+            movingPart.setRight(false);
+        }
+    }
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+        shapex[0] = (float) (x + Math.cos(radians) * 8);
+        shapey[0] = (float) (y + Math.sin(radians) * 8);
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * 8);
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+}
