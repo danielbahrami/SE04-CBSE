92eb4b05c9ad8d08caa36f5b50444baeae1c423c
Daniel Bahrami <daniel@bahrami.dk>
2022-03-28 02:19:33 +0200

    Add NetBeansLab1

---
+++ NetBeansLab1/Collision/src/main/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetector.java
@@ -0,0 +1,42 @@
+package dk.sdu.mmmi.cbse.collisionsystem;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import org.openide.util.lookup.ServiceProvider;
+@ServiceProvider(service = IPostEntityProcessingService.class)
+public class CollisionDetector implements IPostEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity entity : world.getEntities()) {
+            for (Entity collisionDetection : world.getEntities()) {
+                LifePart entityLife = entity.getPart(LifePart.class);
+                if (entity.getID().equals(collisionDetection.getID())) {
+                    continue;
+                }
+                if (this.Collides(entity, collisionDetection)) {
+                    if (entityLife.getLife() > 0) {
+                        entityLife.setLife(entityLife.getLife() - 1);
+                        entityLife.setIsHit(true);
+                        if (entityLife.getLife() <= 0) {
+                            world.removeEntity(entity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public Boolean Collides(Entity entity, Entity entity2) {
+        PositionPart entMov = entity.getPart(PositionPart.class);
+        PositionPart entMov2 = entity2.getPart(PositionPart.class);
+        float dx = (float) entMov.getX() - (float) entMov2.getX();
+        float dy = (float) entMov.getY() - (float) entMov2.getY();
+        float distance = (float) Math.sqrt(dx * dx + dy * dy);
+        if (distance < (entity.getRadius() + entity2.getRadius())) {
+            return true;
+        }
+        return false;
+    }
+}
