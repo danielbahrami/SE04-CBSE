0519744810847cdabd6843ed1280e60353b885a6
Daniel Bahrami <daniel@bahrami.dk>
2022-05-07 14:16:17 +0200

    Update LifePart.java

--- SpringLab/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/LifePart.java
+++ SpringLab/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/LifePart.java
@@ -4,12 +4,13 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 
 public class LifePart implements EntityPart {
-    private boolean dead = false;
     private int life;
     private boolean isHit = false;
+    private float expiration;
 
-    public LifePart(int life) {
+    public LifePart(int life, float expiration) {
         this.life = life;
+        this.expiration = expiration;
     }
 
     public int getLife() {
@@ -20,7 +21,7 @@ public class LifePart implements EntityPart {
         this.life = life;
     }
 
-    public boolean isHit() {
+    public boolean isIsHit() {
         return isHit;
     }
 
@@ -28,18 +29,19 @@ public class LifePart implements EntityPart {
         this.isHit = isHit;
     }
 
-    public boolean isDead() {
-        return dead;
+    public float getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(float expiration) {
+        this.expiration = expiration;
+    }
+
+    public void reduceExpiration(float delta) {
+        this.expiration -= delta;
     }
 
     @Override
     public void process(GameData gameData, Entity entity) {
-        if (isHit) {
-            life = -1;
-            isHit = false;
-        }
-        if (life <= 0) {
-            dead = true;
-        }
     }
 }
