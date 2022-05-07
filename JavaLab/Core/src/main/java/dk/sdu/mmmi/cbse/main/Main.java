e0436fe89bd0684d600384eb3f3e7939d1ea4703
Daniel Bahrami <daniel@bahrami.dk>
2022-05-07 21:38:45 +0200

    Change resolution

--- JavaLab/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ JavaLab/Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Asteroids";
-        cfg.width = 500;
-        cfg.height = 400;
+        cfg.width = 800;
+        cfg.height = 600;
         cfg.useGL30 = false;
         cfg.resizable = false;
         new LwjglApplication(new Game(), cfg);
